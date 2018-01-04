c916668aaf5c4fb7a2a0bfdf66c87eb4b05cf8d4
Leblayd <leblaydavid7@gmail.com>
2018-01-04 10:38:18 +0100

    replaced global variable counts with data from a txt file

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, redirect, request
+from data_manager import *
 
 app = Flask(__name__)
-
-counts = {'GET': 0, 'POST': 0, 'PUT': 0, 'DELETE': 0}
 
 
 @app.route('/')
 def home_page():
+    counts = get_dict_from_file()
     return render_template("main.html", counts=counts)
 
 
 @app.route('/request-counter', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def request_counter():
-    global counts
+    counts = get_dict_from_file()
     counts[request.method] += 1
+    write_dict_to_file(counts)
     return redirect('/')
 
 
